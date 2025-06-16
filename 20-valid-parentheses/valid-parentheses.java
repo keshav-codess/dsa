ed6b8c1716cfcad2cbc386bf58ce67cefbfb073c
keshav-codess <kc397439@gmail.com>
2025-06-16 20:22:20 +0530

    Time: 2 ms (97.14%) | Memory: 41.7 MB (81.57%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()) return false;
+
+                char open = stack.pop();
+                if ((ch == ')' && open != '(') ||
+                    (ch == ']' && open != '[') ||
+                    (ch == '}' && open != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
