33f55c3afad5e4bc90111c74a5d3724491fab3cc
keshav-codess <kc397439@gmail.com>
2025-06-18 15:04:16 +0530

    Time: 8 ms (75.13%) | Memory: 44.1 MB (43.32%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int number = 0;
+        int sign = 1; // 1 means positive, -1 means negative
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                number = number * 10 + (ch - '0');
+            } else if (ch == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (ch == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (ch == '(') {
+                // Push current result and sign to stack
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (ch == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();  // sign
+                result += stack.pop();  // previous result
+            }
+        }
+
+        result += sign * number; // add any remaining number
+        return result;
+    }
+}
