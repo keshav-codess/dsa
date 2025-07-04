1498e8427b14ef804d3e10eb25d105d3a1ca043e
Keshav <kc397439@gmail.com>
2025-07-04 09:54:32 +0530

    Update subarray-sum-equals-k.java

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,36 +1,38 @@
-// class Solution {
-//     public int subarraySum(int[] nums, int k) {
-//         int n = nums.length;
-//         int count = 0;
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
 
-//         // Step 1:calculating prefix sum
-//         int[] prefix = new int[n];
-//         prefix[0] = nums[0];
-//         for (int i = 1; i < n; i++) {
-//             prefix[i] = prefix[i - 1] + nums[i];
-//         }
+        // Step 1:calculating prefix sum
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
 
-//         // Step 2: try all subarrays using prefix sums
-//         for (int i = 0; i < n; i++) {
-//             for (int j = i; j < n; j++) {
-//                 int sum;
-//                 if (i == 0) {
-//                     sum = prefix[j];
-//                 } else {
-//                     sum = prefix[j] - prefix[i - 1];
-//                 }
+        // Step 2: try all subarrays using prefix sums
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int sum;
+                if (i == 0) {
+                    sum = prefix[j];
+                } else {
+                    sum = prefix[j] - prefix[i - 1];
+                }
 
-//                 if (sum == k) {
-//                     count++;
-//                 }
-//             }
-//         }
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
 
-//         return count;
-//     }
-// }
+        return count;
+    }
+}
 
-import java.util.HashMap;
+
+
+// Approach -2 : Using HashMap
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
