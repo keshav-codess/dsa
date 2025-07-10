2e9756bfd9a3c8fa91ff8264477756c74904260a
Keshav <kc397439@gmail.com>
2025-07-10 15:12:58 +0530

    Time: 2 ms (99.80%) | Memory: 50 MB (31.66%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+
+        int numIslands = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // start DFS when you find land
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j); 
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i, j - 1); // left
+        dfs(grid, i, j + 1); // right
+    }
+}
