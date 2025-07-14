eba92da8d3a83785e7cf6b2748fdd97dcfbf52ac
Keshav <kc397439@gmail.com>
2025-07-14 18:56:26 +0530

    Create GFG - Dijkstra Algorithm.java

---
+++ GFG - Dijkstra Algorithm.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] dijkstra(int V, int[][] edges, int src) {
+        List<List<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj.get(u).add(new int[]{v, w});
+            adj.get(v).add(new int[]{u, w}); // undirected graph
+        }
+
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.offer(new int[]{src, 0});
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int u = curr[0], d = curr[1];
+
+            if (d > dist[u]) continue; 
+
+            for (int[] neighbor : adj.get(u)) {
+                int v = neighbor[0], weight = neighbor[1];
+                if (dist[u] + weight < dist[v]) {
+                    dist[v] = dist[u] + weight;
+                    pq.offer(new int[]{v, dist[v]});
+                }
+            }
+        }
+
+        return dist;
+    }
+}
