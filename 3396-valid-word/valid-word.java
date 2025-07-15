7ca715a26117cd5e02865ff9c726baa2e7fc82b1
Keshav <kc397439@gmail.com>
2025-07-15 21:43:10 +0530

    Time: 1 ms (99.12%) | Memory: 41.9 MB (80.13%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch)) return false;
+
+            if (Character.isLetter(ch)) {
+                char lower = Character.toLowerCase(ch);
+                if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
