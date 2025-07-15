86011e480b11fa2a790c1fa94e8a397ff6d75df1
Keshav <kc397439@gmail.com>
2025-07-15 21:51:32 +0530

    Create GFG - Postfix Evaluation.java

---
+++ GFG - Postfix Evaluation.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int evaluate(String[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : arr) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = applyOperator(a, b, token);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop(); 
+    }
+
+    private boolean isOperator(String s) {
+        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
+    }
+
+    private int applyOperator(int a, int b, String op) {
+        switch (op) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b; 
+            default: throw new IllegalArgumentException("Invalid operator: " + op);
+        }
+    }
+}
