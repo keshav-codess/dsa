0c602f866fed1d275134fa265fb01898680d1cc1
Keshav <kc397439@gmail.com>
2025-09-14 03:49:37 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (85.84%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int maxVowel = 0, maxConsonant = 0;
+        String vowels = "aeiou";
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                char ch = (char) ('a' + i);
+                if (vowels.indexOf(ch) >= 0) {
+                    maxVowel = Math.max(maxVowel, freq[i]);
+                } else {
+                    maxConsonant = Math.max(maxConsonant, freq[i]);
+                }
+            }
+        }
+        return maxVowel + maxConsonant;
+    }
+}
