caf859469e1c74788f45c190500823d421972041
Keshav <kc397439@gmail.com>
2025-11-12 18:30:06 +0530

    Time: 6 ms (4.11%) | Memory: 44.6 MB (8.22%) - LeetSync

---
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int minOperations(int[] nums) {
+    final int n = nums.length;
+    final int ones = (int) Arrays.stream(nums).filter(num -> num == 1).count();
+    if (ones > 0)
+      return n - ones;
+
+    int minOps = Integer.MAX_VALUE;
+
+    for (int i = 0; i < n; ++i) {
+      int g = nums[i];
+      for (int j = i + 1; j < n; ++j) {
+        g = gcd(g, nums[j]);
+        if (g == 1) { 
+          minOps = Math.min(minOps, j - i);
+          break;
+        }
+      }
+    }
+
+    return minOps == Integer.MAX_VALUE ? -1 : minOps + n - 1;
+  }
+
+  private int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+  }
+}
